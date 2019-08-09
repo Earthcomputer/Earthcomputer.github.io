7be73caab6c016b7d00aa3bbeb4b9aa2fdf4fc18
Joe <burtonjae@hotmail.co.uk>
2019-08-09 15:41:49 +0100

    Improve maze generation algorithm

--- scripts/maze.js
+++ scripts/maze.js
@@ -1,15 +1,19 @@
 "use strict";
 
-var maze = [];
-var mazeWidth = 32;
-var mazeHeight = 32;
+let maze = [];
+const mazeWidth = 32;
+const mazeHeight = 32;
+window.maze = maze;
+window.mazeWidth = mazeWidth;
+window.mazeHeight = mazeHeight;
 
 // Generate maze
+// This algorithm was the work of many physics lessons many years ago. I don't know if it's unique (probably not).
 {
     // Put walls everywhere
-    for (var x = 0; x < mazeWidth * 2 + 1; x++) {
+    for (let x = 0; x < mazeWidth * 2 + 1; x++) {
         maze[x] = [];
-        for (var y = 0; y < mazeHeight * 2 + 1; y++) {
+        for (let y = 0; y < mazeHeight * 2 + 1; y++) {
             maze[x][y] = x % 2 === 1 && y % 2 === 1;
         }
     }
@@ -18,57 +22,103 @@ var mazeHeight = 32;
     maze[1][mazeHeight * 2] = true;
     maze[mazeWidth * 2 - 1][0] = true;
 
-    // Randomized Prim's from a random starting position
-    var processedTiles = new Set();
-    var nextTilesSet = new Set();
-    var nextTiles = [{x: Math.floor(Math.random() * mazeWidth), y: Math.floor(Math.random() * mazeHeight)}];
-    nextTilesSet.add(JSON.stringify(nextTiles[0]));
+    // Actual maze generation
+    let visited = [];
+    for (let x = 0; x < mazeWidth; x++) {
+        visited[x] = [];
+        for (let y = 0; y < mazeHeight; y++) {
+            visited[x][y] = false;
+        }
+    }
+    let leftToVisit = mazeWidth * mazeHeight;
 
-    while (nextTiles.length !== 0) {
-        var index = Math.floor(Math.random() * nextTiles.length);
-        var curTile = nextTiles[index];
-        nextTiles.splice(index, 1);
-        nextTilesSet.delete(JSON.stringify(curTile));
+    const NORTH = 0, EAST = 1, SOUTH = 2, WEST = 3;
+    const dx = [0, 1, 0, -1];
+    const dy = [-1, 0, 1, 0];
+    // maze generation parameters
+    const turnChance = 0.25; // how often a path turns on average
+    const forkChance = 0.15; // how often a path forks on average
+    const deadEndChance = 0.02; // how often a path randomly stops
 
-        var possibleAttachments = [];
+    let visit = function(x, y, dir) {
+        if (!visited[x][y])
+            leftToVisit--;
+        visited[x][y] = true;
 
-        if (curTile.x !== 0)
-            possibleAttachments.push({x: curTile.x - 1, y: curTile.y});
-        if (curTile.y !== 0)
-            possibleAttachments.push({x: curTile.x, y: curTile.y - 1});
-        if (curTile.x !== mazeWidth - 1)
-            possibleAttachments.push({x: curTile.x + 1, y: curTile.y});
-        if (curTile.y !== mazeHeight - 1)
-            possibleAttachments.push({x: curTile.x, y: curTile.y + 1});
+        // find available directions
+        let availableDirs = [];
+        if (y !== 0 && !visited[x][y-1])
+            availableDirs.push(NORTH);
+        if (x !== mazeWidth - 1 && !visited[x+1][y])
+            availableDirs.push(EAST);
+        if (y !== mazeHeight - 1 && !visited[x][y+1])
+            availableDirs.push(SOUTH);
+        if (x !== 0 && !visited[x-1][y])
+            availableDirs.push(WEST);
 
-        for (var i = 0; i < possibleAttachments.length; i++) {
-            if (!processedTiles.has(JSON.stringify(possibleAttachments[i]))) {
-                if (!nextTilesSet.has(JSON.stringify(possibleAttachments[i]))) {
-                    nextTiles.push(possibleAttachments[i]);
-                    nextTilesSet.add(JSON.stringify(possibleAttachments[i]));
-                }
-                possibleAttachments.splice(i, 1);
-                i--;
+        // give up if we're stuck, or if randomly a dead end
+        if (availableDirs.length === 0 || Math.random() < deadEndChance)
+            return;
+
+        // decide nextDir, based on turnChance
+        let nextDir;
+        if (availableDirs.includes(dir)) {
+            nextDir = dir;
+            if (availableDirs.length > 1 && Math.random() < turnChance) {
+                let index = Math.floor(Math.random() * (availableDirs.length - 1));
+                if (index >= availableDirs.indexOf(dir))
+                    index++;
+                nextDir = availableDirs[index];
             }
+        } else {
+            nextDir = availableDirs[Math.floor(Math.random() * availableDirs.length)];
         }
 
-        if (possibleAttachments.length !== 0) {
-            var adjTile = possibleAttachments[Math.floor(Math.random() * possibleAttachments.length)];
-            maze[((curTile.x * 2 + 1) + (adjTile.x * 2 + 1)) / 2][((curTile.y * 2 + 1) + (adjTile.y * 2 + 1)) / 2] = true;
-        }
+        // break the pathway
+        maze[x * 2 + 1 + dx[nextDir]][y * 2 + 1 + dy[nextDir]] = true;
 
-        processedTiles.add(JSON.stringify(curTile));
+        // recursively visit next spot
+        visit(x + dx[nextDir], y + dy[nextDir], nextDir);
+
+        // randomly fork based on forkChance (revisit = fork)
+        if (Math.random() < forkChance)
+            visit(x, y, dir);
+    };
+
+    // initial visit
+    visit(Math.floor(Math.random() * mazeWidth), Math.floor(Math.random() * mazeHeight), Math.floor(Math.random() * 4));
+
+    while (leftToVisit > 0) {
+        // find a visited cell with an adjacent unvisited cell
+        let found = false;
+        do {
+            let x = Math.floor(Math.random() * mazeWidth);
+            let y = Math.floor(Math.random() * mazeHeight);
+            if (visited[x][y]) {
+                for (let dir = 0; dir < 4; dir++) {
+                    if (dir === NORTH && y === 0) continue;
+                    if (dir === EAST && x === mazeWidth - 1) continue;
+                    if (dir === SOUTH && y === mazeHeight - 1) continue;
+                    if (dir === WEST && x === 0) continue;
+                    if (!visited[x + dx[dir]][y + dy[dir]]) {
+                        visit(x, y, dir);
+                        found = true;
+                        break;
+                    }
+                }
+            }
+        } while (!found);
     }
 }
 
 
-var drawMaze = function(canvas) {
-    var ctx = canvas.getContext("2d");
+let drawMaze = function(canvas) {
+    let ctx = canvas.getContext("2d");
     ctx.strokeStyle = "#F00BAA";
     ctx.lineWidth = 1;
     ctx.beginPath();
-    for (var x = 0; x < mazeWidth * 2 + 1; x++) {
-        for (var y = 0; y < mazeHeight * 2 + 1; y++) {
+    for (let x = 0; x < mazeWidth * 2 + 1; x++) {
+        for (let y = 0; y < mazeHeight * 2 + 1; y++) {
             if (!maze[x][y]) {
                 if (x !== 0 && !maze[x-1][y]) {
                     ctx.moveTo(x * 8 + 4, y * 8 + 4);
